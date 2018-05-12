9b750ddbcbe24fe9286474ff3494261d8bb7dab8
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-12 10:48:53 +0200

    Deleted files for installing mongodb (Raspbian not supported)

--- db/uninstall.sh
+++
@@ -1,7 +0,0 @@
-# Stop the service
-sudo service mongod stop
-# Remove package
-sudo apt-get purge mongodb-org*
-# Remove databases and log files
-sudo rm -r /var/log/mongodb
-sudo rm -r /var/lib/mongodb
