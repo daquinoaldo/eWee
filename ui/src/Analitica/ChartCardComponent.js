d9ae476823dc0fff608c8c54ae3c35d66f5b9229
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-15 16:31:14 +0200

    Trying to resolve a merge 3

---
+++ ui/src/Analitica/ChartCardComponent.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default class ChartCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      api_url: props.url
+    }
+  }
+
+  componentDidMount() {
+    this.timeTick = setInterval(this.fetchData, 1000);
+    var ctx = document.getElementById("the-chart").getContext('2d');
+  }
+
+  fetchData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(result=>result.json())
+    .then(items=>{ this.updateData(items) });
+  }
+
+  updateData = (items) => {
+    console.log(items);
+  }
+
+  render() {
+    return (
+      <div className="mdc-card wrapper">
+        <canvas id="the-chart" width="100" height="100"></canvas>
+        <div className="card__primary">
+          <h2 className="card__title mdc-typography--headline6">Our Changing Planet</h2>
+          <h3 className="card__subtitle mdc-typography--subtitle2">by Kurt Wagner</h3>
+        </div>
+        <div className="card__secondary mdc-typography--body2">
+          Visit ten places on our planet that are undergoing the biggest changes today.
+        </div>
+      </div>
+  )};
+}
