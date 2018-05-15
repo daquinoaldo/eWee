8a1b7824ac7b9d1ff07d2fad27b60caff26d8ae1
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-15 12:44:13 +0200

    UUIDs moved to network, is not a database pertinence

---
+++ network/UUIDs.js
@@ -0,0 +1,40 @@
+const UUIDs = {
+  "temp": "2A1F",
+  "humidity": "2A6F",
+  "light": "2A77",
+  "pir": "2AC5",
+  "door": "0000",
+  "carbon": "0001"
+}
+
+function UUIDToProperty(uuid) {
+
+}
+
+function propertyToUUID(property) {
+
+}
+
+///////// TODO: è solo una bozzada rivedere, da usare per cnvertire gli oggetti della rete in oggetti db e vice versa
+function UUIDsToProperties(obj) {
+  const dbObj = {};
+  for (const key in obj)
+    dbObj[UUIDToProperty(key)] = obj[key];
+  return dbObj;
+}
+
+function propertiesToUUIDs(obj) {
+  const dbObj = {};
+  for (const key in obj)
+    if (key in UUIDs)
+      dbObj[propertyToUUID(key)] = obj[key];
+    else dbObj[key] = obj[key];
+  return dbObj;
+}
+//////////////////////////////////////////////////
+
+module.exports = {
+  UUIDs : UUIDs,
+  UUIDToProperty: UUIDToProperty,
+  propertyToUUID: propertyToUUID
+}
