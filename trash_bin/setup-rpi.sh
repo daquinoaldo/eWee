a07590dca3578474645389088bbe42f85f2606b2
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-15 12:43:10 +0200

    Cleanings: moved unused files in trash_bin for further user

--- trash_bin/setup-rpi.sh
+++ trash_bin/setup-rpi.sh
@@ -4,4 +4,4 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get dist-upgrade
 # Install NodeJS and git
-sudo apt-get install -y nodejs git
+sudo apt-get install -y nodejs git mongodb                                       
