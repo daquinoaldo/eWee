1b3102c4fd3895349b2dcaf523d37ecd08781914
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-21 15:39:14 +0200

    Updated deploy script

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo docker build --no-cache -t mcps/atlante database
-sudo docker build --no-cache -t mcps/api .
+sudo docker build --no-cache -t mcps/atlante atlante.dockerfile
+sudo docker build --no-cache -t mcps/api api.dockerfile
