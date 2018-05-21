00c6d3db8cdb2bb556b21ea38b82738d8a0a4a92
samueleunipi <samueleunipi@gmail.com>
2018-05-21 21:41:15 +0200

    Un buon commit

--- ui/src/Management/ManagementSection.js
+++ ui/src/Management/ManagementSection.js
@@ -5,23 +5,39 @@ import {MDCRipple} from '@material/ripple';
 import SensorChip from './SensorComponent';
 
 var sensors = ['abc', '123', '456', '678']
+const url = 'https://api.p1.aldodaquino.com'
 
 export default class ManagementSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      unboundDevices: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(url+'/home')
+    .then(response => response.json())
+    .then(json => {
+      this.setState({ unboundDevices: json.unboundDevices });
+    });
+  }
 
   sensorsHtml = () => {
-    var s = [];
-    for (var i = 0; i<sensors.length; i++) {
-      s.push(
+    const shtml = [];
+    const unbounded = this.state.unboundDevices;
+    for (var i = 0; i<unbounded.length; i++) {
+      shtml.push(
         <div className="sensor-flex-item" key={i}>
-          <SensorChip uuid={sensors[i]}/>
+          <SensorChip uuid={unbounded[i]}/>
         </div>
       );
     }
-    return s;
+    return shtml;
   }
 
   roomsHtml = () => {
-    
+
   }
 
   render() {
