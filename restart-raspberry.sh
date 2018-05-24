7724238012b79ebb4c741c4bd1439e747080095c
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-24 18:17:03 +0200

    Cleaning of unused scripts

--- restart-raspberry.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-git reset --hard
-rm -rf database/mongo/*
-rm -f database/mongo/*
-git pull
-# This may be NOT a good idea...
-sudo pkill -f sudo
-sudo pkill -f node
-bash start-raspberry.sh
