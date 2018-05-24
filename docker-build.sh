8e627248985a13e09f4b3ce4d6a30c9e56c41f18
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-24 19:21:47 +0200

    Updated docker-build

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sudo docker build --no-cache -t mcps/api -f api.dockerfile .
+sudo docker build --no-cache -t mcps/smartness smartness
 sudo docker build --no-cache -t mcps/ui ui
+
+sudo docker build --no-cache -t mcps/demo demo
