29fcf46673649e5e4a5f34adf7325ec547fc5a81
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-25 20:06:34 +0200

    ALL FILES IN ESTIA MUST BE COMMITTED, INCLUDED BUNDLE: The node image is not able to run npm install on ui folder. So, the bundle files MUST be generated from another PC every time the ui is updated un must be committed with the ui updates.

--- docker-build.sh
+++ docker-build.sh
@@ -6,7 +6,7 @@ sudo docker build --no-cache -t mcps/api -f api.dockerfile .
 sudo docker build --no-cache -t mcps/smartness smartness
 
 #UI
-sudo docker build --no-cache -t mcps/ui ui
+sudo docker build --no-cache -t mcps/ui ui/ESTIA
 
 #DEMO
 sudo docker build --no-cache -t mcps/demo demo
