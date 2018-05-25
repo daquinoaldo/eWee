293b42c1efa0c2d44ebb2d9b8187c052f4088aaa
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-25 19:11:14 +0200

    Dockerfile for UI

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+#API
 sudo docker build --no-cache -t mcps/api -f api.dockerfile .
-sudo docker build --no-cache -t mcps/smartness smartness
-sudo docker build --no-cache -t mcps/ui ui
 
+#Smartness
+sudo docker build --no-cache -t mcps/smartness smartness
+
+#UI
+cd ui
+npm install
+npm run build
+cd ..
+sudo docker build --no-cache -t mcps/ui ESTIA
+
+#DEMO
 sudo docker build --no-cache -t mcps/demo demo
