a96e8fdd86708ef5a17fa942487aa11dcad2173b
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-29 12:54:00 +0200

    Deleted trash_bin

--- trash_bin/setup-rpi.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Add NodeJS repository
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-# Update package and upgrade
-sudo apt-get update
-sudo apt-get dist-upgrade
-# Install NodeJS and git
-sudo apt-get install -y nodejs git mongodb                                       
