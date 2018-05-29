4aa095c07eb30e426a2b0561638a5bb3053132f0
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-05-29 13:16:42 +0200

    Finished the configure-raspberry script, it will executed after the installation

--- configure-raspberry.sh
+++ configure-raspberry.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
+# Install systemds for the services
 sudo bash systemd/install.sh
+sudo bash updater/enable-updater.sh
+#sudo bash demo/install.sh  # you should not install thi service, it generates fake data for testing
+# Install all dependencies
 cd api
 npm install
-#TODO after refactoring
+cd ../network/raspberry-pi
+npm install
+cd ../../ui
+npm install
+# Build the UI statics file
+npm run build
+cd ..
+# Restart to activates the services
 sudo reboot
