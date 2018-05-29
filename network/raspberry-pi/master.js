1dece41f15d60e3d27420df51a32cf1314c1a142
samueleunipi <samueleunipi@gmail.com>
2018-05-29 16:46:06 +0200

    Better comments for master.js

--- network/raspberry-pi/master.js
+++ network/raspberry-pi/master.js
@@ -6,20 +6,25 @@ const Query = require('../../database/query.js').Query;
 
 
 // ----- ----- COSTANTS ----- ----- //
-const ENVIR_SENSING = '181a'; // Our main service
+// Our main service:
+// https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.automation_io.xml
+const AUTOMATION_IO = '181a';
+/*
+ * @note: the 'noble' dependency, used for ble, get stuck on remote invocations
+ *   if the other device goes offline. To work around this problem we've
+ *   introduced timer to each remote call. After 'CONNECTION_TIMEOUT' each
+ *   call is considered 'stuck' and the binded promise rejects the request.
+ */
 const CONNECTION_TIMEOUT = 5000;
-const SEPARET = '\n/// ///// ///// /// \n';
 
 
 // ----- ----- GLOBALS ----- ----- //
-const connectedIDs = {}; // Devices seen
-let pendingActions = []; // Actuators to be set //TODO: check that actions are removed
+// An array of information regarding the devices seen
+const connectedIDs = {};
+// Contains all the action to execute
+let pendingActions = [];
+// Store the timestamp of the last time we've queried for the pending actions
 let lastActionTimestamp = new Date();
-// const pendingActions = [
-//   { 'mac': '30:ae:a4:1c:c2:ee',
-//     'uuid_16': '0003',
-//     'value': 'true'},
-// ];
 
 
 setInterval(() => Query.getActions(lastActionTimestamp).then(actions => {
@@ -33,6 +38,7 @@ setInterval(() => Query.getActions(lastActionTimestamp).then(actions => {
  */
 setInterval(() => noble.startScanning(), 10000);
 
+
 // ----- ----- SETUP AND START ----- ----- //
 async function setup() {
   await Query.init();
@@ -45,6 +51,8 @@ async function setup() {
       noble.startScanning([], true);
     }
   });
+  // It seems that noble stop scanning from time to time
+  setInterval(() => noble.startScanning(), 10000);
 
   /*
    * Manages the device discovery
@@ -57,16 +65,15 @@ async function setup() {
     } // The device is a sample
   });
 
+  /*
+   * Querying actions every 1000 milliseconds (updating what to do)
+   */
   setInterval(() => Query.getActions(lastActionTimestamp).then(actions => {
     console.log(JSON.stringify(actions));
     for (let i = 0; i < actions.length; i++)
       pendingActions.push(reverseTranslator(actions[i]))
     if (actions.length && actions[actions.length-1].timestamp) lastActionTimestamp = actions[actions.length-1].timestamp;
   }), 10000);
-  /*
-   * It seems that noble stop scanning from time to time
-   */
-  setInterval(() => noble.startScanning(), 10000);
 }
 setup();
 
@@ -78,7 +85,7 @@ async function masterLogic (peripheral) {
     await getConnectionPromise(peripheral, CONNECTION_TIMEOUT);
     console.log(peripheral.address + ' (connected)');
   } catch (e) {
-    console.log(SEPARET + '(' + peripheral.address + ')' + "Service discovery error: " + e + SEPARET);
+    console.error('(' + peripheral.address + ') ' + "Service discovery error: " + e);
     delete connectedIDs[peripheral.address];
     peripheral.disconnect();
     return false;
@@ -95,17 +102,21 @@ async function masterLogic (peripheral) {
   } catch (e) {
     console.error(e);
     delete connectedIDs[peripheral.address];
-    peripheral.disconnect((e) => console.error('Error while disconnecting: '+e));
+    peripheral.disconnect((e) => {
+      console.error('('+peripheral.address+') '+'Error while disconnecting: '+e);
+    });
     return false;
   } // Handling error
 
-  // We have found services, need to look for ENVIR_SENSING service
+  // We have found services, need to look for AUTOMATION_IO service
   let sensingService = null;
   for (const i in services) {
     // 0000xxxx-0000-1000-8000-00805F9B34F (128bit representation of 16bit UUID)
     const serviceUUID_16 = services[i].uuid.toString().substring(4, 8);
-    if (serviceUUID_16 === ENVIR_SENSING) { sensingService = services[i]; break; }
-    else console.log(serviceUUID_16);
+    if (serviceUUID_16 === AUTOMATION_IO) {
+      sensingService = services[i];
+      break;
+    } // We've found our service
   }
   // If we haven't our service, let's disconnect and no reconnect anymore
   if(sensingService == null) {
@@ -121,13 +132,13 @@ async function masterLogic (peripheral) {
     characteristicTable = await getCharacteristicPromise(sensingService, CONNECTION_TIMEOUT);
     console.log('2) ' + peripheral.address + ': got char table');
   } catch (e) {
-    console.log(SEPARET + e + SEPARET);
+    console.error('(' + peripheral.address + ') ' + 'Error while getting characteristic table: ' + e);
     delete connectedIDs[peripheral.address];
     peripheral.disconnect();
     return false;
   }
 
-  async function sampleCycle () {
+  async function peripheralCycle () {
     // Trying to retrieving data
     try {
       await getExecutionPromise(peripheral, characteristicTable, CONNECTION_TIMEOUT);
@@ -136,15 +147,16 @@ async function masterLogic (peripheral) {
       console.log(sample);
       Query.insertMeasure(translator(sample));
       // Ensuring to continue sampling
-      setTimeout(sampleCycle, 5000);
+      setTimeout(peripheralCycle, 5000);
     } catch (e) {
-      console.error(SEPARET + e + SEPARET);
+      console.error('(' + peripheral.address + ') ' + 'Error while peripheral cycling: ' + e);
       delete connectedIDs[peripheral.address];
       peripheral.disconnect();
     } // Stop if some error occurs
   }
-  sampleCycle();
+  peripheralCycle();
 }
+
 
 // ----- ----- PROMISES LAND ----- ----- //
 /*
@@ -266,18 +278,20 @@ function getExecutionPromise (peripheral, characteristicTable, timeout) {
 
   return new Promise(async function (resolve, reject) {
     // Finding actions to execute
-    const todo = pendingActions.filter((el) => { return el.device === peripheral.address });
+    const todoIndex = pendingActions.findIndex((el) => { return el.device === peripheral.address });
     // Iterating till there is something to do
-    for (let i = 0; i < todo.length; i++) {
-      const actualAction = todo[i];
-      // Getting the characteristic
+    while (todoIndex != -1) {
+      const actualAction = pendingActions[todoIndex];
+      pendingActions.slice(todoIndex, 1);
+      // Searching for the characteristic in the device
       let newValue = null;
       let characteristic = characteristicTable.find( (el) => {
+        // uuid returns 128bit addresses, we need to convert them into 16 bit
         let uuid_16 = el.uuid.toString().substring(4, 8);
         if (actualAction[uuid_16]!=null) {
           newValue = actualAction[uuid_16];
           return true;
-        } // The characteristic and new value had been found
+        } // The characteristic and new value have been found
         else return false;
       });
 
@@ -285,42 +299,29 @@ function getExecutionPromise (peripheral, characteristicTable, timeout) {
         try {
           // @note: the order in which the actions are executed may be important
           await getWritePromise(characteristic, newValue, timeout);
-          pendingActions.splice(pendingActions.indexOf(actualAction), 1);
         } catch (e) {
           errorsArray.push(e + JSON.stringify(actualAction));
         }
       } // if the characteristic exist, then need to set the actuator
       else {
         errorsArray.push('Characteristic to trigger not found: ' + JSON.stringify(actualAction));
-        pendingActions.splice(pendingActions.indexOf(actualAction), 1);
       } // Otherwise we can delete the pending action
+
+      // Taking next action
+      const todoIndex = pendingActions.findIndex((el) => { return el.device === peripheral.address });
     }
+    // If some error occurred we have to reject
     if (errorsArray.length>0) reject(errorsArray);
     resolve(true);
   });
 }
 
-//TODO: never used
-function getDelayPromise (msec) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve("done!"), msec)
-  })
-}
-
-
-// ----- ----- CALLBACK LAND ----- ----- //
-/*
- * Callback for disconnections events
- */
-//TODO: never used
-function handleDisconnection (peripheral, error) {
-  let devName = peripheral.advertisement.localName;
-  console.log('Disconnected from ' + devName);
-  if (error) console.log(error);
-}
-
 
 // ----- ----- TRANSLATOR ----- ----- //
+/*
+ * Translate an object, created by 'getSamplePromise', containing the measures
+ *   read from the peripherals, into an object usable by the APIs
+ */
 function translator (obj) {
   const newObj = {};
   newObj.id = obj.device;
@@ -332,6 +333,10 @@ function translator (obj) {
   return newObj;
 }
 
+/*
+ * Translate an object returned by 'Query.getActions' (pendingActions) in a
+ *   format easier to use by the master.
+ */
 function reverseTranslator (obj) {
   const newObj = {};
   newObj.device = obj.id;
@@ -343,11 +348,21 @@ function reverseTranslator (obj) {
   return newObj;
 }
 
+/*
+ * Returns true if compatible with our system
+ * @note: our intent for the demo was to use only our devices.
+ *   At the moment this function check only if the ble adv name starts with
+ *   sampler. To make the system fully compatibile with every ble device
+ *   this function has to return true every time
+ */
 function isSampler (peripheral) {
+  /* ** UnCOMMENT FOR FULL COMPADIBILITY  ** */
+  // return true;
+
   // @note: it may happen that name is correct but the mac address in unknown
   if (peripheral.address === 'unknown') return false;
 
   let devname = peripheral.advertisement.localName;
   if (devname==null || devname.length<7) return false;
-  return (devname.substring(0, 7)==='sampler')
+  return (devname.substring(0, 7)==='sampler');
 }
